e4ae06843efcbde9684b04761a565f97e3831da4
ymLuo666 <ymluo@umich.edu>
2026-04-10 20:18:46 +0800

    Add Wasmer deployment copy for SugEx10

---
+++ CISC3003-SugEx10-2026APR10-Wasmer/app/includes/header.inc.php
@@ -0,0 +1,17 @@
+  <header class="mdl-layout__header demo-header">
+      <div class="mdl-layout__header-row">
+         <h1 class="mdl-layout-title"><span>CRM</span> Admin</h1>
+         <div class="mdl-layout-spacer"></div>
+
+         <div class="header-actions">
+            <span id="tt2" class="material-icons mdl-badge mdl-badge--overlap header-action" data-badge="5">account_box</span>
+            <div class="mdl-tooltip" data-mdl-for="tt2">Messages</div>
+
+            <span id="tt3" class="material-icons mdl-badge mdl-badge--overlap header-action" data-badge="4">notifications</span>
+            <div class="mdl-tooltip" data-mdl-for="tt3">Notifications</div>
+
+            <span id="tt4" class="material-icons header-action">search</span>
+            <div class="mdl-tooltip" data-mdl-for="tt4">Search</div>
+         </div>
+    </div>
+  </header>
